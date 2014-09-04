e369f7a26d1524b477290424e769ea3a9139c7b1
Agris <github@ameriks.lv>
2014-09-04 17:05:58 +0300

    Updating legacy result matching to current participants.

--- project/results/utils.py
+++ project/results/utils.py
@@ -4,19 +4,24 @@ from results.models import LegacyResult
 
 
 def map_participants_with_legacy_results():
-    LegacyResult.objects.all().update(participant_2014=None)
-    for participant in Participant.objects.filter(is_participating=True, competition_id=34):
-        competition_id = 11
+    LegacyResult.objects.filter(competition_id=16).update(participant_2014=None)
+    for participant in Participant.objects.filter(is_participating=True, competition_id=35):
+        competition_id = 16
         distance_id = participant.distance_id
         LegacyResult.objects.filter(slug=participant.slug, competition_id=competition_id, distance_id=distance_id).update(participant_2014=participant)
 
 
 def map_participants_with_legacy_results_potential():
-    LegacyResult.objects.all().update(participant_2014_could_be=None, participant_2014_could_be2=None)
-    distance_ids = [28, 29]
-    old_competition_id = 11
+    LegacyResult.objects.filter(competition_id=16).update(participant_2014_could_be=None, participant_2014_could_be2=None)
+    distance_ids = [32, 33, 34]
+    old_ids = {
+        32: 21,
+        33: 23,
+        34: 22
+    }
+    old_competition_id = 16
     for distance_id in distance_ids:
-        old_distance_id = distance_id
+        old_distance_id = old_ids.get(distance_id)
 
         participants = Participant.objects.filter(legacyresult__id=None, is_participating=True, distance_id=distance_id)
         global_slugs = [obj.slug for obj in LegacyResult.objects.filter(competition_id=old_competition_id, distance_id=old_distance_id, participant_2014=None)]
