1c16e7f2ec47ccd3995efcb1bf68ca1416a0a2c6
Ameriks <github@ameriks.lv>
2015-02-14 09:46:31 +0200

    Updating docker run file.

--- .docker/run.sh
+++ .docker/run.sh
@@ -7,32 +7,12 @@ CELERY_WORKERS=${CELERY_WORKERS:-4}
 EXTRA_CMD=${EXTRA_CMD:-}
 EXTRA_REQUIREMENTS=${EXTRA_REQUIREMENTS:-}
 
-SUPERVISOR_CONF=/opt/supervisor.conf
+SUPERVISOR_CONF=/etc/supervisor/conf.d/app.conf
 
 echo "App Components: ${APP_COMPONENTS}"
 
 cd /app
 git pull
-
-touch /var/log/django.log
-chown nginx /var/log/django.log
-
-# supervisor
-cat << EOF > $SUPERVISOR_CONF
-[supervisord]
-nodaemon=false
-
-[unix_http_server]
-file=/var/run//supervisor.sock
-chmod=0700
-
-[rpcinterface:supervisor]
-supervisor.rpcinterface_factory = supervisor.rpcinterface:make_main_rpcinterface
-
-[supervisorctl]
-serverurl=unix:///var/run//supervisor.sock
-
-EOF
 
 if [ -z "$APP_COMPONENTS" ] || [ ! -z "`echo $APP_COMPONENTS | grep app`" ] ; then
     cat << EOF >> $SUPERVISOR_CONF
@@ -53,8 +33,8 @@ user=root
 autostart=true
 autorestart=true
 stopsignal=QUIT
-stdout_logfile=/var/log/uwsgi.log
-stderr_logfile=/var/log/uwsgi.log
+stdout_events_enabled = true
+stderr_events_enabled = true
 
 EOF
 
@@ -63,13 +43,13 @@ fi
 if [ -z "$APP_COMPONENTS" ] || [ ! -z "`echo $APP_COMPONENTS | grep memcached`" ] ; then
     cat << EOF >> $SUPERVISOR_CONF
 [program:memcached]
-command=memcached -m 512 -u nobody -l 0.0.0.0 -I 10m
+command=memcached -m 512 -u nobody -l 0.0.0.0
 autostart=true
 autorestart=true
 user=root
 priority=5
-redirect_stderr=true
-stdout_logfile=/var/log/memcached.log
+stdout_events_enabled = true
+stderr_events_enabled = true
 
 EOF
 fi
@@ -81,12 +61,12 @@ chown nginx /var/log/celeryworker.log
 [program:master-worker]
 priority=99
 directory=/app/project
-command=python manage.py celery worker -B -E -c ${CELERY_WORKERS} --uid=nginx --autoreload
+command=python manage.py celery worker -B -E -c ${CELERY_WORKERS} --autoreload
 user=root
 autostart=true
 autorestart=true
-stdout_logfile=/var/log/celeryworker.log
-stderr_logfile=/var/log/celeryworker.log
+stdout_events_enabled = true
+stderr_events_enabled = true
 
 EOF
 fi
@@ -98,23 +78,21 @@ chown nginx /var/log/celeryworker.log
 [program:worker]
 priority=99
 directory=/app/project
-command=python manage.py celery worker -E -c ${CELERY_WORKERS} --uid=nginx --autoreload
+command=python manage.py celery worker -E -c ${CELERY_WORKERS} --autoreload
 user=root
 autostart=true
 autorestart=true
-stdout_logfile=/var/log/celeryworker.log
-stderr_logfile=/var/log/celeryworker.log
+stdout_events_enabled = true
+stderr_events_enabled = true
 
 EOF
 fi
 
 if [ -z "$APP_COMPONENTS" ] || [ ! -z "`echo $APP_COMPONENTS | grep duplicity`" ] ; then
-    cat << EOF >> /etc/cron.daily/duplicity
-#!/bin/sh
-duplicity --encrypt-key ${BACKUP_BUCKET_ENC_KEY} --exclude /mnt/velo_media/gallery --exclude /mnt/velo_media/easy_thumbnails --archive-dir /app/.duplicity/ --full-if-older-than 30D "/mnt/velo_media" ${BACKUP_BUCKET} > /var/log/duplicity.log
-EOF
-chmod +x /etc/cron.daily/duplicity
 
+ if ! grep -q duplicity "/etc/crontab"; then
+   echo "5 5 * * * root duplicity --encrypt-key ${BACKUP_BUCKET_ENC_KEY} --exclude /mnt/velo_media/gallery --exclude /mnt/velo_media/easy_thumbnails --archive-dir /app/.duplicity/ --full-if-older-than 30D "/mnt/velo_media" ${BACKUP_BUCKET}" >> /etc/crontab
+ fi
     cat << EOF >> $SUPERVISOR_CONF
 [program:cron]
 command = cron -f -L 15
@@ -122,7 +100,8 @@ startsecs = 5
 stopwaitsecs = 3600
 stopasgroup = false
 killasgroup = true
-stdout_logfile=/var/log/cron.log
+stdout_events_enabled = true
+stderr_events_enabled = true
 
 EOF
 
@@ -142,4 +121,4 @@ if [ -z "$APP_COMPONENTS" ] || [ ! -z "`echo $APP_COMPONENTS | grep nginx`" ] ; 
     nginx
 fi
 
-supervisord -c $SUPERVISOR_CONF -n
+supervisord -c /etc/supervisor/supervisord.conf -n
