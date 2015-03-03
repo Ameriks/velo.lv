ca1d3904177928e2eeed1eb54bb9f4eb4a62ab76
Ameriks <github@ameriks.lv>
2015-03-03 15:23:57 +0200

    Changed competition order.

--- project/registration/forms.py
+++ project/registration/forms.py
@@ -33,7 +33,7 @@ class ApplicationCreateForm(RequestKwargModelFormMixin, forms.ModelForm):
         super(ApplicationCreateForm, self).__init__(*args, **kwargs)
 
         now = timezone.now()
-        competitions = Competition.objects.filter(Q(complex_payment_enddate__gt=now) | Q(price__end_registering__gt=now, price__start_registering__lte=now)).distinct().order_by('competition_date')
+        competitions = Competition.objects.filter(Q(complex_payment_enddate__gt=now) | Q(price__end_registering__gt=now, price__start_registering__lte=now)).distinct().order_by('complex_payment_enddate', 'competition_date')
         self.fields['competition'].choices = [(c.id, c) for c in competitions]
 
     def save(self, commit=True):
