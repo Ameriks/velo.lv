ee21dd3dc848851bece4e932f1e73cc27aefd9e3
Ameriks <github@ameriks.lv>
2015-08-02 23:35:41 +0300

    no message

--- project/news/admin.py
+++ project/news/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-
+from django.db import models
 # Register your models here.
-from news.models import Notification
+from redactor.widgets import AdminRedactorEditor
+from news.models import Notification, News
+
+
+class NewsAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+            models.TextField: {'widget': AdminRedactorEditor},
+    }
 
 admin.site.register(Notification)
+admin.site.register(News, NewsAdmin)
+
+
