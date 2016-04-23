16729c610011ae917b3b676d7a5451aeba93cac2
Agris Ameriks <github@pd.lv>
2016-04-23 15:11:53 +0300

    AWS changed endpoint

--- project/velo/settings/production.py
+++ project/velo/settings/production.py
@@ -4,6 +4,8 @@ from .base import *
 
 AWS_SES_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SES_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+#AWS_SES_REGION_NAME = 'eu-west-1'
+AWS_SES_REGION_ENDPOINT = 'email.eu-west-1.amazonaws.com'
 
 DATABASES = {
     'default': {
