04665523aa974765280996afb693ae88d440745b
Agris Ameriks <github@pd.lv>
2016-04-29 15:27:22 +0300

    temp remove ssl redirect.

--- config/settings/common.py
+++ config/settings/common.py
@@ -83,7 +83,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'velo.velo.middleware.SSLRedirectMiddleware', # CUSTOM
+ #   'velo.velo.middleware.SSLRedirectMiddleware', # CUSTOM
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
