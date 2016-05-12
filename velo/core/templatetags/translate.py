3be453c1b1901252f68780d116670067fd20d5ae
Agris Ameriks <github@pd.lv>
2016-05-12 23:51:55 +0300

    no message

--- velo/core/templatetags/translate.py
+++ velo/core/templatetags/translate.py
@@ -11,13 +11,10 @@ def change_lang(context, lang=None, *args, **kwargs):
     Get active page's url by a specified language
     Usage: {% change_lang 'en' %}
     """
-
-    path = context['request'].path
-    url_parts = resolve(path)
-
-    url = path
     cur_language = get_language()
     try:
+        path = context['request'].path
+        url_parts = resolve(path)
         activate(lang)
         url = reverse(url_parts.view_name, kwargs=url_parts.kwargs)
     except:
