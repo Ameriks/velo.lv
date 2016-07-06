e908ee33c312eb7c9e2ff6a9c1a864894fc76f81
Agris Ameriks <github@pd.lv>
2016-07-06 19:28:45 +0300

    A bit changed bug fix.

--- velo/registration/competition_classes/seb2016.py
+++ velo/registration/competition_classes/seb2016.py
@@ -4,7 +4,7 @@ from difflib import get_close_matches
 import datetime
 from django.db.models import Sum
 from django.utils import timezone
-from velo.core.models import Log
+from velo.core.models import Log, Distance
 from velo.registration.competition_classes.base import SEBCompetitionBase
 from velo.registration.models import Application, ChangedName, PreNumberAssign, Number, Participant, UCICategory
 from django import forms
@@ -183,7 +183,7 @@ class Seb2016(SEBCompetitionBase):
             application = Application.objects.get(id=application_id)
 
         if application.participant_set.filter(distance_id=self.SPORTA_DISTANCE_ID):
-            distance = self.competition.distance_set.get(id=self.SPORTA_DISTANCE_ID)
+            distance = Distance.objects.get(id=self.SPORTA_DISTANCE_ID)
             return (('sport_approval', forms.BooleanField(label=_("I am informed that participation in %s requires LRF licence. More info - %s") % (distance, "http://lrf.lv/index.php/licences/licences-2015"), required=True)), )
 
         return ()
