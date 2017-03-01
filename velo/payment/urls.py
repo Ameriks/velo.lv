e6cf82750db2a70a9cdf6129c5b79a80566a5bc5
Eriks <eriks@karls.lv>
2017-03-01 13:36:54 +0200

    Invoice download url removed from language dependant and translatable links.

--- velo/payment/urls.py
+++ velo/payment/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import url
 from django.utils.translation import ugettext_lazy as _
 
-from velo.payment.views import CheckPriceView, InvoiceDownloadView, TransactionRedirectView
+from velo.payment.views import CheckPriceView, TransactionRedirectView
 
 urlpatterns = [
                        url(_(r'^checkprice/(?P<pk>\d+)/$'), CheckPriceView.as_view(), name='check_price'),
 
-                       url(_(r'^invoice/(?P<slug>.+)/$'), InvoiceDownloadView.as_view(), name='invoice_pdf'),
 
                        url(_(r'^transaction/(?P<slug>.+)/$'), TransactionRedirectView.as_view(), name='transaction'),
 ]
