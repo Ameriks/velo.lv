ad21c4cc83a310c3e1eb5b1eb8e06c7f130365cd
Agris Ameriks <github@pd.lv>
2017-03-09 17:41:47 +0200

    Removed height=100 and changed rm2017 distance ids.

--- velo/registration/competition_classes/rm2017.py
+++ velo/registration/competition_classes/rm2017.py
@@ -4,8 +4,8 @@ from velo.registration.competition_classes import RM2016
 class RM2017(RM2016):
     SPORTA_DISTANCE_ID = 65
     TAUTAS_DISTANCE_ID = 66
-    TAUTAS1_DISTANCE_ID = 67
     GIMENU_DISTANCE_ID = 68
+    BERNU_DISTANCE_ID = 67
 
     def _update_year(self, year):
         return year + 3
