a4e791ddd3ba66e4a22c28aa6933d33650742c4c
Agris <agris@pd.lv>
2019-11-25 13:10:01 +0200

    Removed image from invoice

--- velo/core/pdf.py
+++ velo/core/pdf.py
@@ -455,9 +455,10 @@ class InvoiceCanvas(canvas.Canvas):
     #     self.drawImage(img, 0.5*cm, 24 * cm, width=self._pagesize[0]-cm, height=153, preserveAspectRatio=True)
 
     def draw_footer(self):
-        img = os.path.join(settings.MEDIA_ROOT, "adverts", "2019_invoice_footer.jpg")
+        pass
+        # img = os.path.join(settings.MEDIA_ROOT, "adverts", "2019_invoice_footer.jpg")
         # img = "velo/test_media/2019_invoice_footer.jpg"
-        self.drawImage(img, 0.5*cm, 0.5*cm, width=self._pagesize[0]-cm, height=86, preserveAspectRatio=True)
+        # self.drawImage(img, 0.5*cm, 0.5*cm, width=self._pagesize[0]-cm, height=86, preserveAspectRatio=True)
 
 
 def fill_page_with_image(path, canvas):
